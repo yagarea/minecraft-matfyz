93b1a43490d38e39f8dde673bdaf20c5094ef2c9
Jan Černý <jc@ucw.cz>
2023-05-18 23:08:08 +0200

    make player info scraper

---
+++ mffcraft/src/main/java/cz/blackblog/tools/PlayerInfo.java
@@ -0,0 +1,44 @@
+package cz.blackblog.tools;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.*;
+import java.io.*;
+
+public class PlayerInfo {
+
+    public static String getUUID(String name) throws McUUIDNotFoundException {
+        return getProfile(name).getString("id");
+    }
+
+    public static JSONObject getProfile(String nickname) {
+        StringBuilder response = new StringBuilder();
+        try {
+            URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + nickname);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()))) {
+                for (String line; (line = reader.readLine()) != null; ) {
+                    response.append(line);
+                }
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Malformed URL");
+        } catch (IOException e) {
+            throw new RuntimeException("IOException");
+        }
+
+        if (response.length() == 0) {
+            throw new RuntimeException("Empty response");
+        }
+
+        JSONObject json = new JSONObject(response.toString());
+        if (json.has("errorMessage")) {
+            throw new RuntimeException("Error: " + json.getString("errorMessage"));
+        } else {
+            return json;
+        }
+    }
+}
