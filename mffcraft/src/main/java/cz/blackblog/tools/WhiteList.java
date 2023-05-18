e64fae3cfa56e4a23a5abe7681f4295a745a51a2
Jan Černý <jc@ucw.cz>
2023-05-18 23:08:18 +0200

    implement whitelist class

---
+++ mffcraft/src/main/java/cz/blackblog/tools/WhiteList.java
@@ -0,0 +1,31 @@
+package cz.blackblog.tools;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class WhiteList {
+    String whiteListFilePath;
+
+    public WhiteList(String pathToFile) {
+        this.whiteListFilePath = pathToFile;
+    }
+
+    private JSONObject readWhiteList() throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get(this.whiteListFilePath)));
+        return new JSONObject(content);
+    }
+
+    private void writeWhiteList(JSONObject json) throws IOException {
+        Files.write(Paths.get(this.whiteListFilePath), json.toString().getBytes());
+    }
+
+    public void addPlayer(String name) throws IOException {
+        JSONObject json = this.readWhiteList();
+        JSONObject playerInfo = PlayerInfo.getProfile(name);
+        json.append("", playerInfo); // append to root array
+        this.writeWhiteList(json);
+    }
+}
