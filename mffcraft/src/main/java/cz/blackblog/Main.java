519551ef1ee7377fc8ffab7dd06f9cdd9ccc187e
Jan Černý <jc@ucw.cz>
2023-05-21 22:46:49 +0200

    add arguments parsing

--- mffcraft/src/main/java/cz/blackblog/Main.java
+++ mffcraft/src/main/java/cz/blackblog/Main.java
@@ -4,6 +4,13 @@ import cz.blackblog.website.WebServer;
 
 public class Main {
     public static void main(String[] args) {
-        WebServer.launch("127.0.0.1", 8080, "src/static/users", "src/static/whitelist.json");
+        if (args.length == 0) {
+            WebServer.launch("127.0.0.1", 8080, "src/static/users", "src/static/whitelist.json");
+        } else if (args.length != 4) {
+            System.out.println("Usage: java -jar <jarfile> <address> <port> <userDatabase> <whiteListFile>");
+            System.exit(1);
+        }else{
+            WebServer.launch(args[0], Integer.parseInt(args[1]), args[2], args[3]);
+        }
     }
 }
