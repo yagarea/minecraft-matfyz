32649c3d7b955d86e8d6605c32c2894dc2152f0f
Jan Černý <jc@ucw.cz>
2023-05-21 22:34:21 +0200

    create PlayerAddtionStatus class

---
+++ mffcraft/src/main/java/cz/blackblog/website/PlayerAdditionStatus.java
@@ -0,0 +1,24 @@
+package cz.blackblog.website;
+
+public class PlayerAdditionStatus {
+    private final String message;
+    private final boolean isOk;
+
+    public PlayerAdditionStatus() {
+        this.message = "Player was successfully added";
+        this.isOk = true;
+    }
+
+    public PlayerAdditionStatus(String message) {
+        this.message = message;
+        this.isOk = false;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isOk() {
+        return isOk;
+    }
+}
